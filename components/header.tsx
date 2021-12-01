438887d854c6794b4dda6d6f7d4dc763a62aaf2b
Norbert Gocze <gnorbi951@gmail.com>
2021-12-01 22:42:41 +0100

    Add theme-ui

---
+++ components/header.tsx
@@ -0,0 +1,16 @@
+
+type HeaderProps = {
+  className: string;
+}
+
+function Header(props: HeaderProps) {
+  return (
+    <>
+      <header>
+        HEAD
+      </header>   
+    </>
+  );
+}
+
+export default Header;
