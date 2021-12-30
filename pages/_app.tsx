78aa414bf204da671228112183c5d1a67ea8f34b
Norbert Gocze <gnorbi951@gmail.com>
2021-12-30 14:25:26 +0100

    Move title to _app as suggested by compiler

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Head } from 'next/document';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <title>NC Webshop</title>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>NC Webshop</title>
           <link href='http://fonts.cdnfonts.com/css/menlo' rel='stylesheet' />
         </Head>
         <body>
