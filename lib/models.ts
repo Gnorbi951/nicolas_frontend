14905d9578d9818d123a57f1231cc18696c19d9d
Norbert Gocze <gnorbi951@gmail.com>
2021-12-30 14:03:44 +0100

    Add shopitem

--- lib/models.ts
+++ lib/models.ts
@@ -2,3 +2,10 @@ export interface MenuItem {
     path: string;
     label: string;
 }
+
+export interface ShopItem {
+    id: string;
+    name: string;
+    description?: string;
+    imageUrl: string;
+}
